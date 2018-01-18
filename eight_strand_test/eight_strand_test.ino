ca81ab2fbfa8c80509883568286482a8003164d7
Emmett Framson <framsone@gmail.com>
2018-01-17 16:03:10 -0800

    Further refinements to grid tool; Added generic fn template to C++ code which may or may not work; Added fn based on this template to C++ code, which also may or may not work. Wheeeeeeeeeee!

--- eight_strand_test/eight_strand_test.ino
+++ eight_strand_test/eight_strand_test.ino
@@ -477,6 +477,95 @@ void travellingQuadrantPattern() {
 
 }
 
+void fourQuadrants(int num_times_to_do = 1, uint32_t array_of_colors[] = rainbow, int num_colors = TOTAL_COLORS, int wait = 500) {
+
+  //num_times_to_do should be the main criterion to satisfy. Colors and num_shapes should both be able to independently adapt
+  int i = 0;
+  int j = 0;
+  int num_shapes = 4;
+  int num_pixels_in_each_shape = 16;
+
+  xypair shape_1[num_pixels_in_each_shape] = {
+    {7,0},{7,1},{7,2},{7,3},
+    {6,0},{6,1},{6,2},{6,3},
+    {5,0},{5,1},{5,2},{5,3},
+    {4,0},{4,1},{4,2},{4,3}
+  };
+
+  xypair shape_2[num_pixels_in_each_shape] = {
+    {7,4},{7,5},{7,6},{7,7},
+    {6,4},{6,5},{6,6},{6,7},
+    {5,4},{5,5},{5,6},{5,7},
+    {4,4},{4,5},{4,6},{4,7}
+  };
+
+  xypair shape_3[num_pixels_in_each_shape] = {
+    {3,4},{3,5},{3,6},{3,7},
+    {2,4},{2,5},{2,6},{2,7},
+    {1,4},{1,5},{1,6},{1,7},
+    {0,4},{0,5},{0,6},{0,7}
+  };
+
+  xypair shape_4[num_pixels_in_each_shape] = {
+    {3,0},{3,1},{3,2},{3,3},
+    {2,0},{2,1},{2,2},{2,3},
+    {1,0},{1,1},{1,2},{1,3},
+    {0,0},{0,1},{0,2},{0,3}
+  };
+
+
+  xypair *all_shapes[num_shapes] = {
+    shape_1,
+    shape_2,
+    shape_3,
+    shape_4
+  };
+
+  for( i = 0; i < (num_shapes * num_times_to_do); i++ ) {
+    turnStripsOff();
+    for( j = 0; j < num_shapes; j++ ) {
+      setPixelColorAtCoords(all_shapes[i % num_shapes][j].x, all_shapes[i % num_shapes][j].y, array_of_colors[i % num_colors]);
+    }
+    showAllStrips();
+    delay(wait);
+  }
+
+}
+
+/*
+void TEMPLATE_repeatAPatternXNumberOfTimesWithYColors(int num_times_to_do = 1, uint32_t array_of_colors[] = rainbow, int num_colors = TOTAL_COLORS, int wait = 500) {
+
+  //num_times_to_do should be the main criterion to satisfy. Colors and num_shapes should both be able to independently adapt
+  int i = 0;
+  int j = 0;
+  int num_shapes = 0;
+  int num_pixels_in_each_shape = 0;
+
+  xypair shape_1[num_pixels_in_each_shape] = {
+
+  };
+
+  xypair shape_2[num_pixels_in_each_shape] = {
+
+  };
+
+  xypair *all_shapes[num_shapes] = {
+    shape_1,
+    shape_2
+  };
+
+  for( i = 0; i < (num_shapes * num_times_to_do); i++ ) {
+    turnStripsOff();
+    for( j = 0; j < num_shapes; j++ ) {
+      setPixelColorAtCoords(all_shapes[i % num_shapes][j].x, all_shapes[i % num_shapes][j].y, array_of_colors[i % num_colors]);
+    }
+    showAllStrips();
+    delay(wait);
+  }
+
+}
+*/
+
 void loop() {
   checkerboardPattern(5);
   travellingQuadrantPattern();
